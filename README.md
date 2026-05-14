0fe077d3edce3a63898907d6b1ae9c86cd518835
alfonsodelavega <alfonso.delavega@unican.es>
2026-05-14 08:47:01 +0200

    Add branches support to generation

--- README.md
+++ README.md
@@ -10,7 +10,14 @@ From the root of this repository, run:
 python reprogit.py example
 ```
 
-The previous command generates a new git repository in a `repo` folder. This repository has one commit for each one of the `c*` folders present in the `example` directory. Each folder contains the updated files that have changed with respect to the previous commit, as well as a `.message` file that contains the commit message.
+The previous command generates a new git repository in a `repo` folder. This repository has one commit for each one of the folders present in the `example` directory, organized as follows:
+
+- The name of each folder follows the pattern `<commit_number>--<branch_name>`. The `commit_number` orders commits in time, independently of the branch they are placed on, and all commit numbers must start with a "c" (e.g. `c010`).
+- Commits will be placed in branches according to `branch_name`, in the order imposed by their `commit_number`. This is useful to create conflicts between branches, for instance.
+- If no `branch_name` is present, it is assumed that the commit belongs to the `main` default branch.
+- Each folder contains the updated files that have changed with respect to the previous commit, as well as a `.message` file that contains the commit message.
+- If a `.remote` file with a repo url is present at the root folder, then branches will be associated with remote branches, and (WARNING!!) force-pushed.
+
 
 ## Note on commit hashes
 
--- reprogit.py
+++ reprogit.py
@@ -16,14 +16,22 @@ if os.path.exists(output_repo):
 # init a git repository in the output directory
 os.system(f"git init {output_repo}")
 
-# Find all directories starting with 'c' in the current directory
+# Find all directories starting with 'c' in the root directory
 directories = [d for d in glob.glob(os.path.join(root_dir, 'c*/')) if os.path.isdir(d)]
 directories.sort()
 
 # create a commit for each directory
+branches = set()
 for directory in directories:
+    directory_name = os.path.basename(directory[:-1]) # remove the trailing slash
+    branch_name = directory_name.split("--")[1] if "--" in directory_name else "main"
+
+    # Create the branch if it does not exist
+    os.system(f"git -C {output_repo} switch {branch_name} 2>/dev/null || git -C {output_repo} switch -c {branch_name}")
+
     # Copy the contents of the directory to the output repo
     os.system(f"cp -r {directory}/* {output_repo}/")
+
     # Add the files to the git index
     os.system(f"git -C {output_repo} add .")
 
@@ -31,3 +39,26 @@ for directory in directories:
     with open(directory + ".message", "r") as f:
         message = f.read().strip()
         os.system(f"git -C {output_repo} commit -m '{message}'")
+print()
+
+# execute show-branches to show the resulting branches and commits
+print("Branches and commits in the resulting repository:")
+os.system(f"git -C {output_repo} show-branch --all")
+
+print()
+# If a .remote file is present at the root folder, then branches will be associated with remote branches, and (WARNING!!) force-pushed.
+if os.path.exists(os.path.join(root_dir, ".remote")):
+    with open(os.path.join(root_dir, ".remote"), "r") as f:
+        remote_url = f.read().strip()
+        # WARNING!! force-push the resulting repository to the remote url
+        # Show a warning message before pushing
+        print(f"The resulting repository will be configured with a remote to {remote_url}.")
+        print("WARNING: Any force-push to this remote may overwrite existing branches and commits. Make sure you have a backup of any important data before proceeding.")
+        response = input("Enter `yes` to continue, anything else to abort: ").strip().lower()
+        if response != "yes":
+            print("Aborting.")
+            sys.exit(0)
+        print(f"Setting up remote {remote_url}...")
+        os.system(f"git -C {output_repo} remote add origin {remote_url}")
+        print(f"All ready. Now you can push the resulting repository to the remote with the following command: ")
+        print(f"git -C {output_repo} push --all --force")
